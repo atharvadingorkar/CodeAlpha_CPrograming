511efa847c352a7093cf6ebf412b313945e8004c
atharvadingorkar <atharvadingorkar9@gmail.com>
2026-04-19 12:30:28 +0530

    Add files via upload

---
+++ Calculatorr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("Enter operation (+, -, *, /): ");
+    scanf(" %c", &op);
+
+    switch(op) {
+        case '+':
+            printf("Addition = %d", a + b);
+            break;
+
+        case '-':
+            printf("Subtraction = %d", a - b);
+            break;
+
+        case '*':
+            printf("Multiplication = %d", a * b);
+            break;
+
+        case '/':
+            if(b != 0)
+                printf("Division = %d", a / b);
+            else
+                printf("Division by zero not allowed");
+            break;
+
+        default:
+            printf("Invalid operation");
+    }
+
+    return 0;
+}
